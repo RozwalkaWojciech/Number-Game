101049dc3cf67966c9b2bbffb3166a27d6392f82
Wojciech <wojciechrozwalka@gmail.com>
2020-12-12 21:54:09 +0100

    add play method

--- src/com/javer/game/Game.java
+++ src/com/javer/game/Game.java
@@ -4,12 +4,15 @@ import java.util.Scanner;
 
 public class Game {
 
-    private Scanner scanner = new Scanner(System.in);
     private int randomNumber = (int) (Math.random() * 100) + 1;
     private boolean win;
 
     public static void play() {
-
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 10; i > 0; i--) {
+            System.out.println("Try to guess this number! \nYou have: " + i + " chance!");
+            int guess = scanner.nextInt();
+        }
     }
 
 }
