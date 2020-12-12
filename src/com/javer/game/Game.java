260f015f287d21959bf1462b1fa5ac8fe1dde9be
Wojciech <wojciechrozwalka@gmail.com>
2020-12-12 21:05:41 +0100

    add game class

---
+++ src/com/javer/game/Game.java
@@ -0,0 +1,15 @@
+package com.javer.game;
+
+import java.util.Scanner;
+
+public class Game {
+
+    private Scanner scanner = new Scanner(System.in);
+    private int randomNumber = (int) (Math.random() * 100) + 1;
+    private boolean win;
+
+    public static void play() {
+
+    }
+
+}
--- src/com/javer/menu/Menu.java
+++ src/com/javer/menu/Menu.java
@@ -1,8 +1,17 @@
 package com.javer.menu;
 
+import com.javer.game.Game;
+
 public class Menu {
 
     public static void start() {
         System.out.println("Welcome in Number Game!");
+        showMenu();
+    }
+
+    static void showMenu() {
+        System.out.println("You must guess random number between 1 and 100.");
+        System.out.println("You have 10 chance!");
+        Game.play();
     }
 }
